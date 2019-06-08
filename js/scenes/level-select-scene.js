6097bd1794d7c4adff062b332878f3abf8158338
Ryan Radford <werkncode@gmail.com>
2019-06-08 15:55:20 -0700

    added keybindings for level select

--- js/scenes/level-select-scene.js
+++ js/scenes/level-select-scene.js
@@ -31,7 +31,7 @@ export default class LevelSelectScene extends Phaser.Scene {
         //add level select buttons
         this.level1Button = new TextButton(this,
             this.sys.canvas.width * 0.5, this.sys.canvas.height * 0.35,
-            'Level 1: XXXX',
+            '(1) Level 1: XXXX',
             { fill: '#0f0', align: 'center' },
             //key:'game' = platformer-scene.js
             //we provide scene data (level to load) that can be read from init(data) { ... }
@@ -39,25 +39,25 @@ export default class LevelSelectScene extends Phaser.Scene {
 
         this.level2Button = new TextButton(this,
             this.sys.canvas.width * 0.5, this.sys.canvas.height * 0.40,
-            'Level 2: XXXX',
+            '(2) Level 2: XXXX',
             { fill: '#0f0', align: 'center' },
             () => this.scene.start("game", { level: 2 }));
 
         this.level3Button = new TextButton(this,
             this.sys.canvas.width * 0.5, this.sys.canvas.height * 0.45,
-            'Level 3: XXXX',
+            '(3) Level 3: XXXX',
             { fill: '#0f0', align: 'center' },
             () => this.scene.start("game", { level: 3 }));
 
         this.level4Button = new TextButton(this,
             this.sys.canvas.width * 0.5, this.sys.canvas.height * 0.50,
-            'Level 4: XXXX',
+            '(4) Level 4: XXXX',
             { fill: '#0f0', align: 'center' },
             () => this.scene.start("game", { level: 4 }));
 
         this.level5Button = new TextButton(this,
             this.sys.canvas.width * 0.5, this.sys.canvas.height * 0.55,
-            'Level 5: XXXX',
+            '(5) Level 5: XXXX',
             { fill: '#0f0', align: 'center' },
             () => this.scene.start("game", { level: 5 }));
 
@@ -67,6 +67,26 @@ export default class LevelSelectScene extends Phaser.Scene {
         this.add.existing(this.level3Button);
         this.add.existing(this.level4Button);
         this.add.existing(this.level5Button);
+
+        // keybindings
+        this.input.keyboard.on("keydown_ESC", event => {
+            this.scene.start("title_screen");
+        });
+        this.input.keyboard.on("keydown_ONE", event => {
+            this.scene.start("game", { level: 1 });
+        });
+        this.input.keyboard.on("keydown_TWO", event => {
+            this.scene.start("game", { level: 2 });
+        });
+        this.input.keyboard.on("keydown_THREE", event => {
+            this.scene.start("game", { level: 3 });
+        });
+        this.input.keyboard.on("keydown_FOUR", event => {
+            this.scene.start("game", { level: 4 });
+        });
+        this.input.keyboard.on("keydown_FIVE", event => {
+            this.scene.start("game", { level: 5 });
+        });
     }
 
     update(time, delta) { }
